6205e2e9af09a5e979405079a2066d79ea826799
DevinduDh <3ddevindu@gmail.com>
2021-07-28 00:42:37 +0530

    Update Day-Of-The-Programmer.py

--- Day-Of-The-Programmer.py
+++ Day-Of-The-Programmer.py
@@ -1,4 +1,4 @@
-y = 2017
+y = 1917
 
 if y > 1918:
     if y % 400 == 0 or ((y % 4 == 0) and (y % 100 != 0)):
@@ -8,7 +8,7 @@ if y > 1918:
         result = '13.09.' + str(y)
         print(result)
 
-elif y < 1917:
+elif y <= 1917:
     if y % 4 == 0:
         result = '12.09.' + str(y)
         print(result)
