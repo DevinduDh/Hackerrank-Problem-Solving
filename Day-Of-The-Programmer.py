efafcc00b6433d9cf1601e42cbb9e8af71311bcf
DevinduDh <3ddevindu@gmail.com>
2021-07-28 00:41:55 +0530

    Create Day-Of-The-Programmer.py

---
+++ Day-Of-The-Programmer.py
@@ -0,0 +1,21 @@
+y = 2017
+
+if y > 1918:
+    if y % 400 == 0 or ((y % 4 == 0) and (y % 100 != 0)):
+        result = '12.09.' + str(y)
+        print(result)
+    else:
+        result = '13.09.' + str(y)
+        print(result)
+
+elif y < 1917:
+    if y % 4 == 0:
+        result = '12.09.' + str(y)
+        print(result)
+    else:
+        result = '13.09.' + str(y)
+        print(result)
+
+elif y == 1918:
+    result = '26.09.' + str(y)
+    print(result)
